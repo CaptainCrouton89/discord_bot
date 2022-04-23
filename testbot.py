8c3ee05fa5d4c797b4f58353674d7a9799ca1396
CaptainCrouton89 <rhyneer.silas@gmail.com>
2022-04-22 20:27:38 -0500

    up

--- testbot.py
+++ testbot.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import discord
+import  difflib
+import markdown
 from discord.ext import commands
 try:
     from pilights import blinker, templates
@@ -12,8 +14,19 @@ except:
 with open("discord.key") as f:
     TOKEN = f.readline()
 
+ALARIA_WIKI = "/Users/silasrhyneer/Documents/AlariaWiki/wiki"
+
 description = '''HeartRushBot -- A tool for playing and understanding Heart Rush more-better :)'''
 bot = commands.Bot(command_prefix='!', description=description)
+
+def find_page(query):
+    wiki_files = set()
+    for filename in os.listdir(ALARIA_WIKI):
+        path = os.path.join(ALARIA_WIKI, filename)
+        if (".DS_Store" in path) or ("img" in path) or (".git" in path): continue
+        wiki_files.add(filename.replace(".md", ""))
+
+    return difflib.get_close_matches(query, wiki_files)[0] + ".md"
 
 @bot.event
 async def on_ready():
@@ -50,6 +63,23 @@ async def get_rules(ctx, subject):
             all_subjects = sorted([section.replace(".md", "") for section in os.listdir("rules")])
             all_subjects_text = ', '.join(all_subjects)
             await ctx.send(f"Rules on `{subject}` could not be found. Try a subject in the following list: ```{all_subjects_text}```")
+
+@bot.command()
+async def wiki(ctx, subject):
+    """Query the wiki for information on a subject
+    """
+    try:
+        page = find_page(subject)
+        print(page)
+        html = markdown.markdown(open(f'{ALARIA_WIKI}/{page}').read())
+        with open("tmp.html") as f:
+            f.write(html)
+        # print(html)
+        await ctx.send(html)
+        # await ctx.send(file=discord.File(rf'tmp.html'))
+    except:
+        all_subjects = sorted([section.replace(".md", "") for section in os.listdir("rules")])
+        await ctx.send(all_subjects)
 
 
 if rpi:
